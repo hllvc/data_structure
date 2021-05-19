a98081a5cf265aa9c142c08ec0488db06f6ddb06
hllvc <hllvc@icloud.com>
2021-05-19 17:56:35 +0200

    push and pop methods for circular queue

--- Zadatak02/source/ArrayQueue.cpp
+++ Zadatak02/source/ArrayQueue.cpp
@@ -1,5 +1,7 @@
 #include "../include/ArrayQueue.hpp"
 
+#include <iostream>
+
 #include "string"
 
 void ArrayQueue::realocate() {	    // method to realocate size of array
@@ -10,7 +12,7 @@ void ArrayQueue::realocate() {	    // method to realocate size of array
   delete to_delete;  // delete old array
 }
 
-bool ArrayQueue::empty() const { return this->size_; }	// check if empty
+bool ArrayQueue::empty() const { return !this->size_; }	 // check if empty
 
 int ArrayQueue::size() const {
   // if size > 0 return it otherwise throw exception
@@ -24,22 +26,24 @@ const int& ArrayQueue::back() const { return this->arr_ptr_[this->HEAD_]; }
 void ArrayQueue::push(const int data) {
   if (this->size_ == this->capacity_) realocate();
   if (this->HEAD_ == -1) this->TAIL_++;
-  if (this->HEAD_ + 1 == this->capacity_) this->HEAD_ = 0;
+  if (this->HEAD_ + 1 == this->capacity_) this->HEAD_ = -1;
   this->arr_ptr_[++this->HEAD_] = data;
   this->size_++;
 }
 
 int ArrayQueue::pop() {
   if (this->empty()) throw std::string("Queue empty!");
-  this->size_--;
   int data;
-  if (this->TAIL_ == this->HEAD_) {
+  if (this->TAIL_ == this->HEAD_ && this->size_ == 1) {
     data = this->arr_ptr_[this->TAIL_];
     this->TAIL_ = -1;
     this->HEAD_ = -1;
   } else if (this->TAIL_ + 1 == this->capacity_) {
     data = this->arr_ptr_[this->TAIL_];
     this->TAIL_ = 0;
+  } else {
+    data = this->arr_ptr_[this->TAIL_++];
   }
+  this->size_--;
   return data;
 }
