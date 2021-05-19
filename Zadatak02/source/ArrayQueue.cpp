d7d206f084747ce12ab76cc520c96c191e3bca18
hllvc <hllvc@icloud.com>
2021-05-19 12:48:17 +0200

    size, front & back methods

--- Zadatak02/source/ArrayQueue.cpp
+++ Zadatak02/source/ArrayQueue.cpp
@@ -1,3 +1,13 @@
 #include "../include/ArrayQueue.hpp"
 
 bool ArrayQueue::empty() const { return (this->size_) ? true : false; }
+
+int ArrayQueue::size() const { return this->size(); }
+
+const int& ArrayQueue::front() const {
+	return *(this->arr_ptr_ + this->TAIL_);
+}
+
+const int& ArrayQueue::back() const {
+	return *(this->arr_ptr_ + this->HEAD_);
+}
